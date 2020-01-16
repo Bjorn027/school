ef67e71fe3e8d3c917e7838c4e81601907c5ff27
Shawn <shawn.boreland@easternstudent.ca>
2020-01-16 11:55:06 -0400

    Added login, cart, with jQuery

--- jQueryProject/wwwroot/js/site.js
+++ jQueryProject/wwwroot/js/site.js
@@ -1,19 +1,60 @@
-﻿var Classic = "Classic"
-var ClassicP = "299.99"
-var Hinges = "Hinges"
-var HingesP = "199.99"
-var Hard = "Hardwood"
-var HardP = "399.99"
+﻿
+var ClassicP = 299.99
+var HingesP = 199.99
+var HardP = 399.99
+
+
 
 function addClassic() {
     //add this to the cart
-    
+    $("#myTable tbody").append(
+    "<tr>" +
+    "<td>Classic</td>" +
+    "<td>" + ClassicP + "</td>" +
+    "</tr>"
+   );
 }
 function addHinges() {
     //add this to the cart
-}
-function addHard() {
-    //add this to the cart
+    $("#myTable tbody").append(
+        "<tr>" +
+        "<td>Hinges</td>" +
+        "<td>" + HingesP + "</td>" +
+        "</tr>"
+    );
 }
 
+function addHard() {
+    //add this to the cart
+    $("#myTable tbody").append(
+        "<tr>" +
+        "<td>Hardwood</td>" +
+        "<td>" + HardP + "</td>" +
+        "</tr>"
+    );
+}
+
+
+
+function hide() {
+    $('#myTable').fadeOut(600)
+}
+function show() {
+    $('#myTable').fadeIn(600)
+}
+
+
+function doLogin() {
+    var user = $('#username').val()
+    var pass = $('#password').val()
+    if (user == "Shawn" && pass == "jQuery") {
+        
+        alert('You are now logged in')
+        $('.clear1 input').val('')
+
+    } else {
+        alert('Wrong username or password')
+        $('.clear1 input').val('')
+    }
+}
 
