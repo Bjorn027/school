85037257193b3889b58eb98b2092bee9acfa323c
Shawn <shawn.boreland@easternstudent.ca>
2020-01-17 11:50:23 -0400

    Added animation to About page, tweeked css

--- jQueryProject/wwwroot/js/site.js
+++ jQueryProject/wwwroot/js/site.js
@@ -48,13 +48,24 @@ function doLogin() {
     var user = $('#username').val()
     var pass = $('#password').val()
     if (user == "Shawn" && pass == "jQuery") {
-        
         alert('You are now logged in')
         $('.clear1 input').val('')
-
-    } else {
-        alert('Wrong username or password')
+    }
+    else {
+        alert('Wrong username and/or password')
         $('.clear1 input').val('')
     }
 }
 
+function randColor () {
+    var font = ["Impact", "Times New Roman", "Trebuchet MS", "Rockwell Nova", "Comic Sans MS", ];
+    var rand = font[Math.floor(Math.random() * font.length)];
+    $('#randomness').css('font-family', rand);
+    var size = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50];
+    var rand2 = size[Math.floor(Math.random() * size.length)];
+    $('#randomness').css('font-size', rand2);
+    var color = ["red", "blue", "gray", "white", "black", "yellow", "green", "pink", "purple", "orange"];
+    var rand3 = color[Math.floor(Math.random() * color.length)];
+    $('#randomness').css('color', rand3);
+    //$('#randomness').animate({opacity: 0.25})
+}
